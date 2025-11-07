0b9a49e4dc20afa79170ea93589b75ac3af98dde
Ashrith H N <161506298+Ashrithhn@users.noreply.github.com>
2025-11-07 19:50:03 +0530

    with learning basics in java did a simple mini_project to guess a random number generated by math.random function

---
+++ Java/StrongBasicsToLearnDeeper/Namaste.java
@@ -0,0 +1,28 @@
+public class Namaste
+{
+    public static void main(String[] args){
+        //function invoking/function calling
+        enu_madudilla();
+    }
+
+
+
+    //function definition
+
+    public static void enu_madudilla()
+    {
+        String messege = "hai";
+        //printmessege("hai");
+        printmessege(messege);
+
+
+
+    }
+
+    static String printmessege(String messege)
+    {
+        System.out.println(messege);
+        return messege;
+    }
+
+}
