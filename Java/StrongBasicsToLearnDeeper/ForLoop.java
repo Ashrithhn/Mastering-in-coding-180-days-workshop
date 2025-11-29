f3c121e210494d05da5251c8addc95c63c71181d
Ashrith H N <161506298+Ashrithhn@users.noreply.github.com>
2025-11-29 11:43:37 +0530

    forloop and nested for loop

---
+++ Java/StrongBasicsToLearnDeeper/ForLoop.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ForLoop {
+    public static void main(String[] args) throws InterruptedException {
+
+        Scanner scanner = new Scanner(System.in);
+//        byte max;
+//        System.out.println("enter the no of time that loop want to continue: ");
+//        max = scanner.nextByte();
+        for (int i = 0; i <= 2; i++){
+            for (int j = 0; j <= 10; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+
+
+//            if(i == 5){
+//                continue;
+//            }
+//            System.out.print(i + " ");
+//           Thread.sleep(1000);
+//        }
+//
+//
+//       for(int i=10; i>1; i--){
+//           System.out.println(i);
+//      }
+    }
+}
